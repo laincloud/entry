fbd9cde7fc471279cd5e7a3b3f168c75ef203f79
supermeng <laomengever@gmail.com>
2017-11-29 15:49:03 +0800

    Expand dependency package version of six

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 requirements = [
     'websocket-client==0.32.0',
     'protobuf==3.0.0b3',
-    'six==1.9.0',
+    'six>=1.9.0',
 ]
 
 setup(
     name="entryclient",
     author='EricPai <ericpai94@hotmail.com>',
-    version='2.3.1',
+    version='2.3.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
